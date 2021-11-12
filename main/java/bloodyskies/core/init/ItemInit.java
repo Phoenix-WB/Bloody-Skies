3d0e0af0994dee4676188b30add447acb4381284
Phoenix-WB <91941086+Phoenix-WB@users.noreply.github.com>
2021-11-12 14:51:50 +1000

    Recipes + New Items
    Recipes for current and new items added, will texture when I'm free probably next week

--- main/java/bloodyskies/core/init/ItemInit.java
+++ main/java/bloodyskies/core/init/ItemInit.java
@@ -11,9 +11,20 @@ public class ItemInit {
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,
 			BloodySkies.MOD_ID);
 
+	//Miscellaneous tab
 	public static final RegistryObject<Item> VILLAGER_HEART = ITEMS.register("villager_heart",
-			() -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+			() -> new Item(new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_MISC)));
+
+	public static final RegistryObject<Item> CRIMSON_ICHOR = ITEMS.register("chrimsom_ichor",
+			() -> new Item(new Item.Properties().stacksTo(8).tab(CreativeModeTab.TAB_MISC)));
+
+	public static final RegistryObject<Item> BLOOD_HILT = ITEMS.register("blood_hilt",
+			() -> new Item(new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_MISC)));
+
+	public static final RegistryObject<Item> QUARTZ_BLADE = ITEMS.register("quartz_blade",
+			() -> new Item(new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_MISC)));
 	
+	//Tools tab
 	public static final RegistryObject<Item> TECPATL = ITEMS.register("tecpatl",
 			() -> new Item(new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_TOOLS)));
 }
